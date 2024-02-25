f2a6e64efc2ac74df1c6e7a224e92a8727a1ad1e
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-02-25 08:54:14 +0530

    Day01

---
+++ Day01/demo1.js
@@ -0,0 +1,27 @@
+// var => scope Global 
+var b1, b2, b_3;
+// let scope => within nearest Block {}
+var a1, a2, a3;
+// const = Scope = > Global as well as local 
+var a$1 = 3.14; // Forward declaration & Defination
+// Data type 
+// number(int,float,double,short,long),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void (it is neutral data )
+// Misc. null & undefined => it is used value as well as data type 
+var c = 10;
+// console.log("Value of c is "+c);
+var str;
+// console.log("Value of str is "+str);
+str = 'Sumit';
+// console.log("Value of str is "+str);
+var myany;
+myany = 2.5;
+// console.log('value of myany is '+myany)
+// myany='Sumit';
+// console.log('value of myany is '+myany)
+// myany=true;
+// console.log('value of myany is '+myany)
+var d;
+var g = 45;
+console.log("\n    Value of g is ".concat(g, "\n"));
---
+++ Day01/demo1.ts
@@ -0,0 +1,42 @@
+// var => scope Global 
+var b1,b2,b_3;
+// let scope => within nearest Block {}
+
+let a1,a2,a3;
+
+// const = Scope = > Global as well as local 
+const a$1=3.14;   // Forward declaration & Defination
+
+// Data type 
+// number(int,float,double,short,long),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void (it is neutral data )
+// Misc. null & undefined => it is used value as well as data type 
+
+let c:number=10; 
+// console.log("Value of c is "+c);
+
+let str!:string;
+// console.log("Value of str is "+str);
+str='Sumit';
+// console.log("Value of str is "+str);
+
+let myany:any;
+myany=2.5;
+
+// console.log('value of myany is '+myany)
+// myany='Sumit';
+// console.log('value of myany is '+myany)
+// myany=true;
+// console.log('value of myany is '+myany)
+
+
+let d;
+
+let g=45;
+
+console.log(`
+    Value of g is ${g}
+`);
+
+ 

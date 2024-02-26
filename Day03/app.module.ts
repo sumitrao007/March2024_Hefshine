5bc190bb6848e9081e29751119cbffd71103bf10
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-02-26 09:56:48 +0530

    Day03

---
+++ Day03/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { StringinterpolationComponent } from './stringinterpolation/stringinterpolation.component';
+import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { StylebindingComponent } from './stylebinding/stylebinding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    StringinterpolationComponent,
+    PropertybindingComponent,
+    StylebindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
